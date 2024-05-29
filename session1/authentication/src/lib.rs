fd599fa72304edbe7dbade5aa137cd634f6c6657
Mack Octavian <maestrooctavian@gmail.com>
2024-05-29 14:16:07 +0300

    Created a world simplest login system

---
+++ session1/authentication/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn greet_user(name: &str) -> String {
+    format!("Hello {name}")
+}
+
+pub fn login(username: &str, password: &str) -> bool {
+    username.to_lowercase() == "admin" && password == "password"
+}
+
+pub fn read_line() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Stdin not working");
+    input.trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_greet_user() {
+        assert_eq!("Hello Mack", greet_user("Mack"));
+    }
+
+    #[test] 
+    fn test_login() {
+        assert!(login("ADMIN", "password"));
+        assert!(!login("not-admin", "password"));
+        assert!(!login("admin", "not-password"));
+    }
+
+}
---
+++ session1/login/src/main.rs
@@ -0,0 +1,28 @@
+use authentication::{login, read_line, greet_user}; 
+
+fn main() {
+    let mut tries = 0;
+
+    loop { 
+        println!("Enter your username: ");
+        let username = read_line();
+        println!("Enter your password");
+        let password = read_line();
+
+        if login(&username, &password) {
+            println!("Welcome, {username}");
+            break;
+        }else {
+            println!("Incorrect username of password");
+            tries += 1;
+
+            if tries >= 3 { 
+                println!("Too many tries");
+                break;
+            }else {
+                let mut tries_remaining = 3 - tries;  
+                println!("{} trial remaining", tries_remaining);
+            }
+        }
+    }
+}

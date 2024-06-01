71765c6542972261d480af37c84489164b6301f3
Mack Octavian <maestrooctavian@gmail.com>
2024-06-02 02:14:26 +0300

    Storing the list of users and password into a hashmap instead of vectors

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -1,30 +1,31 @@
+use std::collections::HashMap;
+
 pub fn greet_user(name: &str) {
     println!("Hello {name}")
 }
 
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let username = username.to_lowercase();
+    let users = get_users();
 
-    if username != "admin" && username != "user" {
-        return None;
+    if let Some(user) = users.get(&username) {
+        if user.password == password {
+           return  Some(LoginAction::Granted(user.role.clone()));
+        }else {
+            return  Some(LoginAction::Denied);
+        }
     }
 
-    if username == "admin" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::Admin))
-    } else if username == "user" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::User))
-    } else {
-        Some(LoginAction::Denied)
-    }
+    None
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginAction {
     Granted(LoginRole),
     Denied,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginRole {
     Admin,
     User,
@@ -35,6 +36,38 @@ pub fn read_line() -> String {
     std::io::stdin().read_line(&mut input).expect("Stdin not working");
     input.trim().to_string()
 }
+
+#[derive(Debug, Clone)]
+pub struct User {
+    pub username: String,
+    pub password: String,
+    role: LoginRole
+}
+
+impl User {
+    pub fn new(username: &str, password: &str, role: LoginRole) -> Self {
+        Self {
+            username: username.to_lowercase(),
+            password: password.to_string(),
+            role
+        }
+    }
+}
+
+// pub fn get_users() -> Vec<User>{
+//     vec![
+//         User::new("admin", "password", LoginRole::Admin),
+//         User::new("user", "password", LoginRole::User)
+//     ]
+// }
+
+fn  get_users() -> HashMap<String, User> {
+    let mut users = HashMap::new();
+    users.insert("admin".to_string(), User::new("admin", "password", LoginRole::Admin),);
+    users.insert("user".to_string(), User::new("user", "password", LoginRole::User));
+    users
+}
+
 
 #[cfg(test)]
 mod tests {
